8112e9f588df6f560ec7b0473aded7bbf6101d81
bob kings <bobkings@protonmail.com>
2024-02-12 21:45:45 +0300

    updates

--- main/settings.py
+++ main/settings.py
@@ -86,10 +86,25 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DBNAME'),
+        'USER': os.environ.get('DBUSERNAME'),
+        'PASSWORD': os.environ.get('DBPASSWORD'),
+        'ATOMIC_REQUESTS': True,
+        'OPTIONS': {
+            'options': '-c search_path={}'.format(os.environ.get('DBSCHEMA'))
+        },
+        'HOST': str(os.environ.get('DBHOST')),
+        'PORT': int(os.environ.get('DBPORT')),
+
     }
 }
 
