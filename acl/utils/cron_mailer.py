af354fe9da9b2369720e0aa0806c035bc1608a6b
bobkings <bobiekings@gmail.com>
2024-03-18 12:48:57 +0300

    updates

--- acl/utils/cron_mailer.py
+++ acl/utils/cron_mailer.py
@@ -2,28 +2,39 @@ import time
 import requests
 from django.db.models import Q
 from acl import models
-from django.core.mail import send_mail
-# exec(open('communication/utils/mailgun_sdk.py').read())
+from django.core.mail import send_mail, BadHeaderError
+# exec(open('acl/utils/cron_mailer.py').read())
 
 def get_emails():
     print("into get emails")
-    emails = models.Sendmail.objects.filter(Q(status='PENDING') | Q(status='FAILED'))[:70]
+    emails = models.Sendmail.objects.filter(Q(status='PENDING') | Q(status='FAILED'))
     print("got emails", len(emails))
     main(emails)
 
 def main(emails):
-	print("Main starting...")
-	count = 0
-	for target in emails:
+    print("Main starting...")
+    count = 0
+      
+    for target in emails:
 		
-		count += 1
-		
-		subject = target.subject
-		message = target.message
-		email = target.email
+        count += 1
 
+        subject = target.subject
+        message = target.message
+        email = target.email
+
+
+            
+        # send = send_mail(subject, message, 'notification@akhskenya.org', email)
+        try:
+            send_mail(subject, message, 'notification@akhskenya.org', email, fail_silently=False)
+            target.status = "SENT"
+            target.save()
+            print(f'{count}-<200>-{email[0]}: Email sent successfully.')
+        except BadHeaderError:
+            print(f'{count}-<400>-{email[0]}: Invalid header found.')
+        except Exception as e:
+            print(f'<500>-An error occurred: {str(e)}')
 			
-		send_mail(subject, message, 'notification@akhskenya.org', email)
-			
-		time.sleep(1)
+        time.sleep(1)
 # get_emails()
