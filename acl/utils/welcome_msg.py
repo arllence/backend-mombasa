c0c9e0b65c9b978d97b2b89df9a1c2daa74c9840
bobkings <bobiekings@gmail.com>
2024-04-02 14:55:44 +0300

    updates

--- acl/utils/welcome_msg.py
+++ acl/utils/welcome_msg.py
@@ -1,7 +1,8 @@
 from acl.models import Sendmail, User
+# exec(open('acl/utils/welcome_msg.py').read())
 
 def main():
-    subject = "Welcome To Quotation System [PSMDQS]. "
+    subject = "Welcome To MMD's Quote Request System [PSMDQS]. "
 
     def set_message(instance):
         message = f"[Platform Access Details]\n\nDear {instance.first_name}, \n\n These are the credentials to MMD's Quote Request System. Username is your  email which is : {instance.email}\n and password is: welcome@123\n To Access the system use this link: http://172.20.0.42/\nIf you encounter any challenge while navigating the platform, please contact IT department at 54030.\n\nKind Regards\nPSMDQS-AKHK\n(**System Generated**)"
