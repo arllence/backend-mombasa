165ee3f328325ccbd0f04e6e9fc3701992483508
bobkings <bobiekings@gmail.com>
2024-04-11 17:18:56 +0300

    updates

--- trs/views.py
+++ trs/views.py
@@ -717,7 +717,7 @@ class TrsViewSet(viewsets.ViewSet):
             payload = request.data
             roles = user_util.fetchusergroups(request.user.id) 
 
-            serializer = serializers.ApprovalSerializer(
+            serializer = serializers.TravelForwardingSerializer(
                     data=payload, many=False)
             
             if serializer.is_valid():
@@ -731,169 +731,66 @@ class TrsViewSet(viewsets.ViewSet):
                 
                 with transaction.atomic():
 
-                    is_hod = False
-                    is_ceo = False
-                    is_slt = False
-                    is_hof = False
-                    is_cash_office = False
-                    is_transport_office = False
-
-                    traveler_status = "APPROVED"
-
                     if send_to == 'HOD':
                         traveler.requires_hod_approval = True
-                        is_hod = True
-                        approval_for = "HOD"
-                        traveler_status = "APPROVED"
+                        emails = list(get_user_model().objects.filter(Q(groups__name='HOD') & Q(department=traveler.department)).values_list('email', flat=True))
                         
                     elif send_to == "SLT":
-                        is_slt = True
-                        approval_for = "SLT"
-                        traveler_status = "APPROVED"
+                        traveler.requires_slt_approval = True
+                        if traveler.department.slt:
+                            emails = [traveler.department.slt.lead.email]
+                        else:
+                            return Response({"details": "Department has no SLT assigned !"}, status=status.HTTP_400_BAD_REQUEST)
 
                     elif send_to == "HOF" :
-                        is_hof = True
-                        approval_for = "HOF"
+                        traveler.requires_hof_approval = True
+                        emails = list(get_user_model().objects.filter(Q(groups__name='HOF')).values_list('email', flat=True))
                         
                     elif send_to == "CEO":
-                        is_ceo = True
-                        approval_for = "CEO"
+                        traveler.requires_ceo_approval = True
+                        emails = list(get_user_model().objects.filter(Q(groups__name='CEO')).values_list('email', flat=True))
 
                     elif send_to == "TRANSPORT":
-                        is_transport_office = True
-                        approval_for = "TRANSPORT"
+                        traveler.requires_transport_approval = True
+                        emails = list(get_user_model().objects.filter(Q(groups__name='TRANSPORT')).values_list('email', flat=True))
 
                     elif send_to == "CASH_OFFICE":
-                        is_cash_office = True
-                        approval_for = "CASH_OFFICE"
+                        traveler.requires_cash_office_approval = True
+                        emails = list(get_user_model().objects.filter(Q(groups__name='CASH_OFFICE')).values_list('email', flat=True))
 
-                        approval_msg = payload.get('text')
-                        if not approval_msg:
-                            return Response({"details": "Amount / Transaction Code / Message Required !"}, status=status.HTTP_400_BAD_REQUEST)
+                    elif send_to == "ADMINISTRATOR":
+                        traveler.requires_administrator_approval = True
+                        emails = list(get_user_model().objects.filter(Q(groups__name='ADMINISTRATOR')).values_list('email', flat=True))
 
-
-                    is_existing = models.Approval.objects.filter(Q(traveler=traveler) & Q(approval_for=approval_for)).exists()
-
-                    raw = {
-                        "traveler": traveler,
-                        "approval_for": approval_for,
-                        "approved_by": authenticated_user,
-                    }  
-
-                    if is_cash_office:
-                        raw.update({"approval_msg": approval_msg})
-
-                    if is_existing:
-                        models.Approval.objects.filter(Q(traveler=traveler) & Q(approval_for=approval_for)).update(**raw)
-                    else:
-                        models.Approval.objects.create(
-                            **raw
-                        )
-
-                    
-                    traveler.budget_code = budget_code
-
-                    if is_hod:
-                        traveler.status = traveler_status
-                        traveler.is_hod_approved = is_hod
-
-                    if is_ceo:
-                        traveler.is_ceo_approved = is_ceo
-
-                    if is_slt:
-                        traveler.is_slt_approved = is_slt
-
-                    if is_hof:
-                        traveler.is_hof_approved = is_hof
-
-                    if is_cash_office:
-                        traveler_status = "CLOSED"
-                        traveler.status = "CLOSED"
-                        traveler.closed_by = authenticated_user
-                        traveler.date_closed = datetime.datetime.now()
-                        traveler.is_cash_office_approved = is_cash_office
-
-                    if is_transport_office:
-                        traveler_status = "CLOSED"
-                        traveler.status = "CLOSED"
-                        traveler.closed_by = authenticated_user
-                        traveler.date_closed = datetime.datetime.now()
-                        traveler.is_transport_dpt_approved = is_transport_office
-
+                    # update travel
                     traveler.save()
 
+
+                    # create forwarding instance
+                    previous_forwarder = models.TravelForwarding.objects.filter(
+                        Q(traveler=traveler)).order_by('-date_created').first()
+                    if previous_forwarder:
+                        previous_forwarder = previous_forwarder.forward_to
+                    else:
+                        previous_forwarder = "CREATOR"
+
                     raw = {
                         "traveler": traveler,
-                        "status": traveler_status,
-                        "action_by": authenticated_user
+                        "forward_to": send_to,
+                        "forward_from": previous_forwarder,
+                        "forward_by": authenticated_user,
                     }
+                    models.TravelForwarding.objects.create(**raw)
 
-                    models.StatusChange.objects.create(**raw)
+                    # Notify recipient
+                    subject = f"Travel Request Received [{traveler.tid}]"
+                    message = f"Hello. \nTravel Request: of ID {traveler.tid}\nhas been forwarded to you by {authenticated_user.first_name} {authenticated_user.first_name},\npending your action.\n\nRegards\nTRS-AKHK"
 
-                    # Notify the requestor
-                    if is_hod:
-                        subject = f"Travel Request {traveler.tid} Status  [TRS-AKHK]"
-                        message = f"Dear {traveler.created_by.first_name}, \nYour Travel Request has been Approved by HOD.\nPending SLT Approval.\n\nRegards\nTRS-AKHK"
-                    elif is_slt:
-                        subject = f"Travel Request {traveler.tid} Status  [TRS-AKHK]"
-                        message = f"Dear {traveler.created_by.first_name}, \nYour Travel Request has been Approved by SLT.\nPending Finance Approval.\n\nRegards\nTRS-AKHK"
-                    elif is_hof:
-                        subject = f"Travel Request {traveler.tid} Status  [TRS-AKHK]"
-                        message = f"Dear {traveler.created_by.first_name}, \nYour Travel Request has been Approved by Finance.\n\nRegards\nTRS-AKHK"
-                    elif is_ceo:
-                        subject = f"Travel Request {traveler.tid} Status  [TRS-AKHK]"
-                        message = f"Dear {traveler.created_by.first_name}, \nYour Travel Request has been Approved by CEO.\n\nRegards\nTRS-AKHK"
-                    elif is_cash_office:
-                        subject = f"Travel Request {traveler.tid} Status  [TRS-AKHK]"
-                        message = f"Dear {traveler.created_by.first_name}, \nYour Travel Request has been Approved by Cash Office.\n\nRegards\nTRS-AKHK"
-                    elif is_transport_office:
-                        subject = f"Travel Request {traveler.tid} Status  [TRS-AKHK]"
-                        message = f"Dear {traveler.created_by.first_name}, \nYour Travel Request has been Approved by Transport Office.\n\nRegards\nTRS-AKHK"
+                    send_mail(subject, message, 'notification@akhskenya.org', emails)
 
-                    send_mail(subject, message, 'notification@akhskenya.org', [traveler.created_by.email])
-
-                    # Notify HOF
-                    if is_slt and traveler_status == 'APPROVED':
-                        emails = list(get_user_model().objects.filter(Q(groups__name='HOF')).values_list('email', flat=True))
-                        subject = f"Request for Travel Budget Approval: {traveler.tid}.  [TRS-AKHK]"
-                        message = f"Hello. \nTravel Request: {traveler.tid} is pending budget approval by CEO/HOF.\n\nRegards\nTRS-AKHK"
-
-                        send_mail(subject, message, 'notification@akhskenya.org', emails)
-
-                    # Notify CEO
-                    if is_hof and traveler.mode_of_transport == 'FLIGHT':
-                        emails = list(get_user_model().objects.filter(Q(groups__name='CEO')).values_list('email', flat=True))
-                        subject = f"Travel Request: {traveler.tid} Pending Your Action.  [TRS-AKHK]"
-                        message = f"Hello. \nTravel Request: {traveler.tid} has been approved by Finance\nand is now pending your approval.\n\nRegards\nTRS-AKHK"
-
-                        send_mail(subject, message, 'notification@akhskenya.org', emails)
-
-                    # Notify ADMINISTRATOR
-                    if is_ceo:
-                        emails = list(get_user_model().objects.filter(Q(groups__name='ADMINISTRATOR')).values_list('email', flat=True))
-                        subject = f"Travel Request: {traveler.tid} Pending Your Action.  [TRS-AKHK]"
-                        message = f"Hello. \nTravel Request: {traveler.tid} has been approved by both HOD/SLT and HOF/CEO, and is now pending administration and costing\n\nRegards\nTRS-AKHK"
-
-                        send_mail(subject, message, 'notification@akhskenya.org', emails)
-
-                    # Notify CASH OFFICE
-                    if is_hof and traveler.mode_of_transport == 'PSV':
-                        emails = list(get_user_model().objects.filter(Q(groups__name='CASH_OFFICE')).values_list('email', flat=True))
-                        subject = f"Travel Request: {traveler.tid} Pending Your Action.  [TRS-AKHK]"
-                        message = f"Hello. \nTravel Request: {traveler.tid} has been approved by finance office and is now pending your action\n\nRegards\nTRS-AKHK"
-
-                        send_mail(subject, message, 'notification@akhskenya.org', emails)
-
-                    # Notify TRANSPORT
-                    if is_hof and traveler.mode_of_transport == 'HOSPITAL VEHICLE':
-                        emails = list(get_user_model().objects.filter(Q(groups__name='TRANSPORT')).values_list('email', flat=True))
-                        subject = f"Travel Request: {traveler.tid} Pending Your Action.  [TRS-AKHK]"
-                        message = f"Hello. \nTravel Request: {traveler.tid} has been approved by finance office and is now pending your action\n\nRegards\nTRS-AKHK"
-
-                        send_mail(subject, message, 'notification@akhskenya.org', emails)
 
                 user_util.log_account_activity(
-                    authenticated_user, traveler.created_by, "Travel Request approval", f"Approval Executed TID: {str(traveler.id)}")
+                    authenticated_user, traveler.created_by, f"Travel Request forwarded to {send_to}", f"Approval Executed TID: {str(traveler.id)}")
                 
                 return Response('success', status=status.HTTP_200_OK)
             
