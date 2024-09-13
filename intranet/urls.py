df0e5e6ee0433518699873f2e1177a7b16494add
bobkings <bobiekings@gmail.com>
2024-09-13 13:40:05 +0300

    updates

--- intranet/urls.py
+++ intranet/urls.py
@@ -7,6 +7,7 @@ router.register('document-manager', views.DocumentManagerViewSet, basename='docu
 router.register('quick-links', views.QuickLinksViewSet, basename='quick-links')
 router.register('generics', views.GenericsViewSet, basename='generics')
 router.register('qips', views.QipsViewSet, basename='qips')
+router.register('intranet-departments', views.DepartmentsViewSet, basename='intranet-departments')
 # router.register('s3-reports',views.ReportsViewSet, basename='s3-reports')
 # router.register('s3-analytics',views.AnalyticsViewSet, basename='s3-analytics')
    
