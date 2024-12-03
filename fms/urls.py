e66c575fbfcdd686accf1f50eba458b97e5c02e1
bobkings <bobiekings@gmail.com>
2024-12-03 12:35:18 +0300

    updates

--- fms/urls.py
+++ fms/urls.py
@@ -5,7 +5,7 @@ router = DefaultRouter(trailing_slash=False)
 
 router.register('fms', views.FmsViewSet, basename='fms')
 # router.register('locums', views.LocumViewSet, basename='locums')
-# router.register('fms-reports',views.SRRSReportsViewSet, basename='fms-reports')
+router.register('fms-reports',views.ReportsViewSet, basename='fms-reports')
 # router.register('fms-analytics',views.SRRSAnalyticsViewSet, basename='fms-analytics')
    
                          
