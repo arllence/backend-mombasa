9d3c920a2408a4f6087f304508332ea573a53fd2
bobkings <bobiekings@gmail.com>
2025-04-10 11:38:05 +0300

    category for Facility

--- mhd/models.py
+++ mhd/models.py
@@ -45,6 +45,7 @@ class EquipmentType(models.Model):
 class Facility(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, default='OHC')
     is_deleted = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
 
