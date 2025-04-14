810599d5a9db50e9667122da2aa706455646519b
bobkings <bobiekings@gmail.com>
2025-04-14 12:00:28 +0300

    updates

--- mhd/views.py
+++ mhd/views.py
@@ -1565,7 +1565,8 @@ class MHSViewSet(viewsets.ViewSet):
                                     expected_closure_datetime__lt=now,
                                     is_deleted=False,
                                     date_assigned__isnull=False,  # avoid unassigned
-                                    date_closed__isnull=True      # only still-open issues
+                                    date_closed__isnull=True,      # only still-open issues
+                                    date_completed__isnull=True
                                 ).order_by('-date_created')
                             else:
                                 resp = models.Issue.objects.annotate(
