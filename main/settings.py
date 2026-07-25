8b1f894f71c44a4673dec0d92f67a8ab55c05337
bobkings <bobiekings@gmail.com>
2026-07-25 07:28:50 +0300

    updates

--- main/settings.py
+++ main/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'attendance.apps.AttendanceConfig',
+    # 'attendance.apps.AttendanceConfig',
     # 'attendance.middleware.CloneCheckMiddleware',
     'attendance.middleware.AttendanceMiddleware'
 ]
